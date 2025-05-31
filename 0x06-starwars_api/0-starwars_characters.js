c972763f9cc59005c4d07a77a4e0a09eef91a2c6
hassanabdisalan <hassanabdisalan3@gmail.com>
2025-05-31 23:20:23 +0200

    Task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  const printCharacter = (index) => {
+    if (index === characters.length) return;
+
+    request(characters[index], (err, res, body) => {
+      if (!err) {
+        const character = JSON.parse(body);
+        console.log(character.name);
+        printCharacter(index + 1);
+      }
+    });
+  };
+
+  printCharacter(0);
+});
